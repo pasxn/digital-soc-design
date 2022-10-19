40110ffe8387931af72e5a1df8fa92a5fd7d3738
Pasan Perera <pasansanjula.android@gmail.com>
2022-10-19 08:52:48 +0530

    update

--- sliit_add/practice/shift_register.vhd
+++ sliit_add/practice/shift_register.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 entity shift_register is
     port(inputt     : in std_logic_vector(3 downto 0);
         outputt     : out std_logic_vector(3 downto 0);
-        clk         : in std_logic;);
+        clk         : in std_logic);
 end shift_register;
 
 architecture rtl of shift_register is
