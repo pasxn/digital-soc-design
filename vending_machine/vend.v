03efbe6c6f2eea955d5f7cb9b5983a54cee5c3c8
pasxn <pasansanjula.android@gmail.com>
2023-06-10 14:24:41 +0530

    vend done

--- vending_machine/vend.v
+++ vending_machine/vend.v
@@ -3,14 +3,14 @@
 module vend (
   input   wire                clk,
   input   wire                reset,
-  // vend
+  // vend siganals
   input   wire                button_pc,
   input   wire                button_hc,
   input   wire                button_cc,
   input   wire                token_in,
   input   wire                dispense_done,
-  output  reg                 coffee_select_done,
-  output  reg [2:0]           coffee_select
+  output  reg [2:0]           coffee_select,
+  output  reg [2:0]           change_tokens
 );
 
 localparam  WAIT_VEND         = 3'd0;
@@ -20,7 +20,7 @@ localparam  TOKEN_THREE       = 3'd3;
 localparam  PLAIN_COFFEE      = 3'd4;
 localparam  HAZELNUT_COFFEE   = 3'd5;
 localparam  COCONUT_COFFEE    = 3'd6;
-localparam  DECODE_VAR        = 3'd7;
+localparam  NO_OPERATION      = 3'd7;
 
 reg [7:0]   state             = WAIT_VEND;
 reg [7:0]   num_tokens        = 0;
@@ -40,7 +40,7 @@ always @(posedge clk) begin
           num_tokens  <= num_tokens + 1;
           state       <= TOKEN_ONE;
         end
-        else if(button_pc | button_hc| button_cc | dispense_done) begin
+        else if(button_pc | button_hc | button_cc | dispense_done) begin
           state   <= WAIT_VEND;
         end
       end
@@ -88,7 +88,57 @@ always @(posedge clk) begin
         else if(dispense_done) begin
           state <= TOKEN_THREE;
         end
-      end                  
+      end
+      PLAIN_COFFEE: begin
+        coffee_select <= 2'b01;
+        change_tokens <= num_tokens - 1;
+        if(button_pc | button_hc | button_cc ) begin
+          state <= PLAIN_COFFEE;
+        end
+        else if(token_in) begin
+          state <= NO_OPERATION;
+        end
+        else if(dispense_done) begin
+          state < WAIT_VEND;
+        end
+      end
+      HAZELNUT_COFFEE: begin
+        coffee_select <= 2'b10;
+        change_tokens <= num_tokens - 2;
+        if(button_pc | button_hc | button_cc ) begin
+          state <= HAZELNUT_COFFEE;
+        end
+        else if(token_in) begin
+          state <= NO_OPERATION;
+        end
+        else if(dispense_done) begin
+          state < WAIT_VEND;
+        end
+      end
+      COCONUT_COFFEE: begin
+        coffee_select <= 2'b11;
+        change_tokens <= num_tokens - 3;
+        if(button_pc | button_hc| button_cc ) begin
+          state <= COCONUT_COFFEE;
+        end
+        else if(token_in) begin
+          state <= NO_OPERATION;
+        end
+        else if(dispense_done) begin
+          state < WAIT_VEND;
+        end
+      end
+      NO_OPERATION: begin
+        if(token_in) begin
+          num_tokens <= num_tokens + 1;
+          state <= NO_OPERATION;
+        end
+        if(button_pc | button_hc | button_cc | dispense_done) begin
+          state <= NO_OPERATION;
+        end
+        change_tokens <= num_tokens;
+        state <= WAIT_VEND;
+      end 
       defualt: begin
         state <= WAIT_VEND;
       end
@@ -96,7 +146,7 @@ always @(posedge clk) begin
   end
 end
 
-// Dump waves
+// dump waves
 `ifdef DEBUG
   initial begin
     $dumpfile("vend.vcd");
